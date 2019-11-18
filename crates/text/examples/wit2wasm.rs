d6ccd5ea073b201090e7d0af5c7b405243aaea32
Alex Crichton <alex@alexcrichton.com>
2019-11-18 12:26:55 -0800

    Run the validator by default in wit2wasm and wasm2wit

--- crates/text/examples/wit2wasm.rs
+++ crates/text/examples/wit2wasm.rs
@@ -6,6 +6,7 @@ fn main() -> anyhow::Result<()> {
     let mut opts = Options::new();
     opts.optopt("o", "", "set output file name", "NAME");
     opts.optflag("h", "help", "print this help menu");
+    opts.optflag("", "no-validate", "don't validate the module");
     let matches = opts.parse(env::args_os().skip(1))?;
     if matches.opt_present("h") {
         print_usage(opts);
@@ -19,6 +20,9 @@ fn main() -> anyhow::Result<()> {
     };
 
     let wasm = wit_text::parse_file(&input)?;
+    if !matches.opt_present("no-validate") {
+        wit_validator::validate(&wasm)?;
+    }
     if let Some(output) = matches.opt_str("o") {
         std::fs::write(&output, wasm).context(format!("failed to write `{}`", output))?;
     }

6a57f32512dcc949eb23f5e277c2797f464be4ca
Alex Crichton <alex@alexcrichton.com>
2019-11-18 12:28:22 -0800

    Include validation in wasm2wit as well

--- crates/printer/examples/wasm2wit.rs
+++ crates/printer/examples/wasm2wit.rs
@@ -1,11 +1,13 @@
 use anyhow::Context;
 use getopts::Options;
 use std::env;
+use std::fs;
 
 fn main() -> anyhow::Result<()> {
     let mut opts = Options::new();
     opts.optopt("o", "", "set output file name", "NAME");
     opts.optflag("h", "help", "print this help menu");
+    opts.optflag("", "no-validate", "don't validate the module");
     let matches = opts.parse(env::args_os().skip(1))?;
     if matches.opt_present("h") {
         print_usage(opts);
@@ -18,7 +20,11 @@ fn main() -> anyhow::Result<()> {
         std::process::exit(1);
     };
 
-    let wit = wit_printer::print_file(&input)?;
+    let bytes = fs::read(&input).context(format!("failed to read: {}", input))?;
+    if !matches.opt_present("no-validate") {
+        wit_validator::validate(&bytes)?;
+    }
+    let wit = wit_printer::print_bytes(&bytes)?;
     if let Some(output) = matches.opt_str("o") {
         std::fs::write(&output, wit).context(format!("failed to write `{}`", output))?;
     } else {

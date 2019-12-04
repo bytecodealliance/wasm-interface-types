723b9670b49d84f2da2e12ca0d6ec76d34f6f589
Alex Crichton <alex@alexcrichton.com>
2019-12-04 14:50:11 -0800

    Allow parsing binaries with `wit-text`
    Follow the precedent of `wat` for this.

--- crates/text/src/lib.rs
+++ crates/text/src/lib.rs
@@ -7,8 +7,10 @@
 
 #![deny(missing_docs)]
 
-use anyhow::Context;
+use anyhow::{bail, Context};
+use std::borrow::Cow;
 use std::path::Path;
+use std::str;
 use wast::parser::ParseBuffer;
 
 mod ast;
@@ -18,26 +20,81 @@ pub use ast::*;
 
 /// Parses a `file` on the filesystem as a textual representation of WebAssembly
 /// Interface Types, returning the binary representation of the module.
+///
+/// Note that the `file` could either be a valid `*.wit`, `*.wat`, or `*.wasm`
+/// file. In the `*.wasm` case the bytes are passed through unmodified.
+///
+/// # Errors
+///
+/// For information about errors, see the [`parse_bytes`] documentation.
 pub fn parse_file(file: impl AsRef<Path>) -> anyhow::Result<Vec<u8>> {
     _parse_file(file.as_ref())
 }
 
 fn _parse_file(file: &Path) -> anyhow::Result<Vec<u8>> {
-    let contents = std::fs::read_to_string(file)
-        .context(format!("failed to read `{}` to a string", file.display()))?;
-    match _parse_str(&contents) {
-        Ok(bytes) => Ok(bytes),
+    let contents = std::fs::read(file).context(format!("failed to read `{}`", file.display()))?;
+    match parse_bytes(&contents) {
+        Ok(bytes) => Ok(bytes.into_owned()),
         Err(mut e) => {
-            e.set_path(file);
-            Err(e.into())
+            if let Some(e) = e.downcast_mut::<wast::Error>() {
+                e.set_path(file);
+            }
+            Err(e)
         }
     }
 }
 
-/// Parses an in-memory string as the textual representation of WebAssembly
-/// interface types and returned the binary serialization of the module.
-pub fn parse_str(wat: impl AsRef<str>) -> Result<Vec<u8>, wast::Error> {
-    _parse_str(wat.as_ref())
+/// Parses in-memory bytes as either the text format or a binary WebAssembly
+/// module.
+///
+/// This function will attempt to interpret the given bytes as one of two
+/// options:
+///
+/// * A utf-8 string which is a `*.wit` file to be parsed.
+/// * A binary WebAssembly file starting with `b"\0asm"`
+///
+/// If the input is a string then it will be parsed as `*.wit`, and then after
+/// parsing it will be encoded back into a WebAssembly binary module. If the
+/// input is a binary that starts with `b"\0asm"` it will be returned verbatim.
+/// Everything that doesn't start with `b"\0asm"` will be parsed as a utf-8
+/// `*.wat` file, returning errors as appropriate.
+///
+/// # Errors
+///
+/// In addition to all of the errors that can be returned from [`parse_str`],
+/// this function will also return an error if the input does not start with
+/// `b"\0asm"` and is invalid utf-8. (failed to even try to call [`parse_str`]).
+pub fn parse_bytes(bytes: &[u8]) -> anyhow::Result<Cow<'_, [u8]>> {
+    if bytes.starts_with(b"\0asm") {
+        return Ok(bytes.into());
+    }
+    let result = match str::from_utf8(bytes) {
+        Ok(s) => _parse_str(s)?,
+        Err(_) => bail!("input bytes aren't valid utf-8"),
+    };
+    Ok(result.into())
+}
+
+/// Parses an in-memory string as the text format, returning the file as a
+/// binary WebAssembly file.
+///
+/// This function is intended to be a stable convenience function for parsing a
+/// `*.wit` file into a WebAssembly binary. This is a high-level operation which
+/// does not expose any parsing internals, for that you'll want to use the
+/// [`Module`] type and the `wast` crate.
+///
+/// # Errors
+///
+/// This function can fail for a number of reasons, including (but not limited
+/// to):
+///
+/// * The `wit` input may fail to lex, such as having invalid tokens or syntax
+/// * The `wit` input may fail to parse, such as having incorrect syntactical
+///   structure
+/// * The `wit` input may contain names that could not be resolved
+///
+pub fn parse_str(wit: impl AsRef<str>) -> Result<Vec<u8>, wast::Error> {
+    _parse_str(wit.as_ref())
 }
 
 fn _parse_str(wat: &str) -> Result<Vec<u8>, wast::Error> {
